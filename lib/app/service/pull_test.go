c38afbed4e5047e5da05109bcf810b38d231bb47
dmitri <deemok@gmail.com>
2021-07-14 22:23:43 +0200

    Forward port uprade via support (https://github.com/gravitational/gravity/pull/1920).

--- lib/app/service/pull_test.go
+++ lib/app/service/pull_test.go
@@ -18,6 +18,7 @@ package service
 
 import (
 	"bytes"
+	"context"
 	"path/filepath"
 	"time"
 
@@ -63,25 +64,24 @@ func (s *PullerSuite) TestPullPackage(c *C) {
 	_, err := s.srcPack.CreatePackage(loc, bytes.NewBuffer([]byte("data")))
 	c.Assert(err, IsNil)
 
-	env, err := PullPackage(PackagePullRequest{
+	puller := app.Puller{
 		FieldLogger: logger,
 		SrcPack:     s.srcPack,
 		DstPack:     s.dstPack,
-		Package:     loc,
-	})
+	}
+	err = puller.PullPackage(context.TODO(), loc)
+	c.Assert(err, IsNil)
+
+	env, err := s.dstPack.ReadPackageEnvelope(loc)
 	c.Assert(err, IsNil)
 	c.Assert(env.Locator, Equals, loc)
 
-	env, err = s.dstPack.ReadPackageEnvelope(loc)
-	c.Assert(err, IsNil)
-	c.Assert(env.Locator, Equals, loc)
-
-	_, err = PullPackage(PackagePullRequest{
+	puller = app.Puller{
 		FieldLogger: logger,
 		SrcPack:     s.srcPack,
 		DstPack:     s.dstPack,
-		Package:     loc,
-	})
+	}
+	err = puller.PullPackage(context.TODO(), loc)
 	c.Assert(trace.IsAlreadyExists(err), Equals, true)
 }
 
@@ -113,14 +113,15 @@ func (s *PullerSuite) pullApp(c *C, parallel int) {
 		Packages: s.dstPack,
 	}, c)
 
-	pulled, err := PullApp(AppPullRequest{
+	puller := app.Puller{
 		SrcPack:  s.srcPack,
 		DstPack:  s.dstPack,
 		SrcApp:   s.srcApp,
 		DstApp:   s.dstApp,
-		Package:  clusterAppLoc,
+		Upsert:   true, // TODO(dima): check, b/c not in upstream
 		Parallel: parallel,
-	})
+	}
+	err := puller.PullApp(context.TODO(), clusterAppLoc)
 	c.Assert(err, IsNil)
 	c.Assert(pulled.Package, Equals, clusterAppLoc)
 
@@ -137,14 +138,14 @@ func (s *PullerSuite) pullApp(c *C, parallel int) {
 	c.Assert(err, IsNil)
 	c.Assert(local.Package, Equals, clusterAppLoc)
 
-	_, err = PullApp(AppPullRequest{
+	puller = app.Puller{
 		SrcPack:  s.srcPack,
 		DstPack:  s.dstPack,
 		SrcApp:   s.srcApp,
 		DstApp:   s.dstApp,
-		Package:  clusterAppLoc,
 		Parallel: parallel,
-	})
+	}
+	err = puller.PullApp(context.TODO(), clusterAppLoc)
 	c.Assert(trace.IsAlreadyExists(err), Equals, true)
 }
 
--- lib/blob/fs/fs.go
+++ lib/blob/fs/fs.go
@@ -122,7 +122,7 @@ func (o *objects) WriteBLOB(data io.Reader) (*blob.Envelope, error) {
 	// then move it to the proper location based on it's hash
 	f, err := ioutil.TempFile(o.config.tempDir(), "blob")
 	if err != nil {
-		return nil, trace.Wrap(err)
+		return nil, trace.ConvertSystemError(err)
 	}
 	defer f.Close()
 
@@ -157,7 +157,7 @@ func (o *objects) WriteBLOB(data io.Reader) (*blob.Envelope, error) {
 	// now place it to the right place in the filesystem
 	targetPath := filepath.Join(targetDir, hash)
 	if err := os.Rename(f.Name(), targetPath); err != nil {
-		defer os.Remove(f.Name())
+		os.Remove(f.Name())
 		return nil, trace.Wrap(err)
 	}
 	if hasUser {
@@ -169,7 +169,7 @@ func (o *objects) WriteBLOB(data io.Reader) (*blob.Envelope, error) {
 	}
 	fileInfo, err := os.Stat(targetPath)
 	if err != nil {
-		if err2 := os.Remove(targetPath); err2 != nil {
+		if err := os.Remove(targetPath); err != nil {
 			log.WithError(err).Errorf("Failed to remove %v.", targetPath)
 		}
 		return nil, trace.ConvertSystemError(err)
